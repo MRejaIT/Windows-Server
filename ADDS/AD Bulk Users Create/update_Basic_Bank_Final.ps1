d0380c3eb27bf682d233ab0285712e1c6834392b
Md. Mohsin Reja <162022058+MRejaIT@users.noreply.github.com>
2024-09-12 18:02:52 +0600

    Add files via upload

---
+++ ADDS/AD Bulk Users Create/update_Basic_Bank_Final.ps1
@@ -0,0 +1,13 @@
+# Import AD Module
+Import-Module ActiveDirectory
+ 
+write-Host 'Starting to update AD Attributes.......' -NoNewline -ForegroundColor Yellow
+# Import CSV into variable $users
+ 
+$users = Import-Csv -Path "D:\Basic Bank\updateusersinformation_Final.csv"
+# Loop through CSV and update users if the exist in CVS file
+ 
+foreach ($user in $users) 
+{Get-ADUser -Filter "userPrincipalName -eq '$($user.userPrincipalName)'" | 
+Set-ADUser -postOfficeBox $($User.postOfficeBox)
+}
